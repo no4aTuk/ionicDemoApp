6e7cf4faf6f3e457ec660968df5e4d61269a91ac
Nikolay <pochati@yandex.ru>
2019-04-15 17:01:28 +0300

    added list page

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,8 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { User } from "../models/user";
+import { IonInfiniteScroll } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {}
+export class Tab2Page {
+  initialUsers: User[] = [
+    new User("bob", 12),
+    new User("alice", 22),
+    new User("scot", 42),
+    new User("andrew", 9),
+    new User("matteo", 14),
+    new User("looper", 19),
+    new User("cristian", 25),
+    new User("miranda", 39),
+    new User("olegsey", 31),
+    new User("kyoto", 29),
+    new User("jeky", 20),
+    new User("jhon", 40),
+    new User("silver", 32)
+  ]
+
+  @ViewChild(IonInfiniteScroll) infiniteScroll: IonInfiniteScroll;
+
+  users: User[] = this.initialUsers;
+
+  constructor() {}
+
+  loadData(event) {
+    setTimeout(() => {
+      console.log('Done');
+      event.target.complete();
+
+      this.users = this.users.concat(this.initialUsers);
+      if (this.users.length == 1000) {
+        event.target.disabled = true;
+      }
+    }, 500);
+  }
+}
